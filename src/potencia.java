35d4399919ddba7e3f7aee5999c742c478eaa5e3
Sam6482 <20243ds141@utez.edu.mx>
2025-09-20 19:04:55 -0600

    Terminando potencia

--- src/potencia.java
+++ src/potencia.java
@@ -1,3 +1,21 @@
 public class potencia {
     
+    public static int Recursivo(int a, int b){
+        if(b == 0){
+           return 1;
+        }
+
+        else {
+               return a * Recursivo(a, b - 1);
+        }
+    }
+
+      public static void main(String[] args) {
+        int base = 2;
+        int exponente = 5;
+        int resultado = Recursivo(base, exponente);
+       
+        System.out.println(base + " elevado a " + exponente + " es: " + resultado);
+
+    }
 }
